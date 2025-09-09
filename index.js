3a9316319ee6791d3e99521faf980b07c4f0cc7c
Brayan Duque <bradumar2016@gmail.com>
2025-09-08 22:28:39 -0500

    implementamos nodemon y lo colocamos en practica

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const express = require("express");
 //Creación de la aplicación express
 const app = express();
 //Definición de una ruta para la raíz del sitio
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.get("/", (req, res) => res.send("Hello, soy una app full-stack!"));
 //Iniciar el servidor en el puerto 3000
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 // Escuchar en el puerto definido
 app.listen(PORT, (error) => {
   if (error) {
     throw error;
   }
-  console.log(`Mi primera app con express - escuchando el puerto ${PORT}!`);
+  console.log(`Mi app con express - escuchando el puerto ${PORT}!`);
 });
