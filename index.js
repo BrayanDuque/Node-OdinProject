7e02992615b09ff4a0f9d559a8f25d5aea3bc2ee
Brayan Duque <bradumar2016@gmail.com>
2025-09-08 22:15:36 -0500

    implemetamos el package.json, además de instalar con npm express

---
+++ index.js
@@ -0,0 +1,15 @@
+//Importación de express
+const express = require("express");
+//Creación de la aplicación express
+const app = express();
+//Definición de una ruta para la raíz del sitio
+app.get("/", (req, res) => res.send("Hello, world!"));
+//Iniciar el servidor en el puerto 3000
+const PORT = 3000;
+// Escuchar en el puerto definido
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Mi primera app con express - escuchando el puerto ${PORT}!`);
+});
